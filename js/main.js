18fd70d6bc00296f528e050f1502232e6c0fad52
3nD7eR <erikod0202@gmail.com>
2021-02-14 18:14:03 +0200

    added chart

--- js/main.js
+++ js/main.js
@@ -22,6 +22,10 @@ document.querySelector('button').addEventListener('click', function() {
 	if (elementDiv != null) {
 		elementDiv.remove();
 	}
+	elementDiv = document.querySelector('.river_chart');
+	if (elementDiv != null) {
+		elementDiv.remove();
+	}
 
 // 	ввод и проверка данных
 	inputNumber1 = document.getElementById('numberIn1').value;
@@ -74,7 +78,7 @@ document.querySelector('button').addEventListener('click', function() {
 		alert('критический уровень должен быть больше начального уровня');
 		return;
 	}
-// 	расчет
+// 	расчет уровней
 	riverLevel = inputNumber1;
 	liftingPercentage = inputNumber2;
 	criticalLevel = inputNumber3;
@@ -84,12 +88,10 @@ document.querySelector('button').addEventListener('click', function() {
 		liftingLevel [i] = liftingLevel [i - 1] + liftingLevel [i - 1] * liftingPercentage / 100;
 		if (liftingLevel [i] >= criticalLevel) break;
 	}
-/*	console.log ('динамика роста уровня реки ' + liftingLevel);
-	console.log('через ' + i + ' ч ' + 'уровень превысит критический уровень  ' + criticalLevel + ' и станет ' + liftingLevel [i]);
-*/
 
-//	выводим результат
+//	выводим результат чертим график
 	getDrawingResults (liftingLevel, criticalLevel);
+	getDrawingChart(liftingLevel, criticalLevel);
 });
 
 function getDrawingPage() {
@@ -124,9 +126,43 @@ function getDrawingResults(liftingLevel, criticalLevel) {
 	}
 	criticalTimeMin = (criticalLevel - liftingLevel [liftingLevel.length - 2]) / ((liftingLevel [liftingLevel.length - 1] - liftingLevel [liftingLevel.length - 2]) / 60);
 	criticalTimeMin = criticalTimeMin.toFixed(0);
-/*	console.log(liftingLevel [liftingLevel.length - 2]);
-	console.log(criticalTimeMin);
-*/
+
 	document.querySelector('main').insertAdjacentHTML('beforeend',
-		`<p class="block1">Динамика роста уровня реки ${levelDynamics}</p>\<p class="block2">Через ${liftingLevel.length - 2} ч ${criticalTimeMin} мин уровень реки достигнит критического уровня ${criticalLevel} м &#9888</p>\<p class="block3">Через ${liftingLevel.length - 1} ч уровень реки превысит критический уровень ${criticalLevel} м на ${lastLevel} м, если не прорвет дамбу &#128526</p>`);
+		`<p class="block1">Динамика роста уровня реки ${levelDynamics}</p>\<p class="block2">Через
+		 ${liftingLevel.length - 2} ч ${criticalTimeMin} мин уровень реки достигнит критического уровня
+		  ${criticalLevel} м &#9888</p>\<p class="block3">Через ${liftingLevel.length - 1} ч уровень реки превысит критический уровень ${criticalLevel} м на ${lastLevel} м, если не прорвет дамбу &#128526</p>`);
+}
+
+function getDrawingChart(liftingLevel, criticalLevel) {
+	let ctx;
+	let lineChart;
+	let labels = [];
+	let drawingCriticalLevel = [];
+	let drawingLiftingLevel = [];
+
+	for (let i = 0; i <= liftingLevel.length - 1; i = i + 1) {
+		labels[i] = i.toString();
+		drawingCriticalLevel [i] = criticalLevel;
+		drawingLiftingLevel [i] = Math.round( liftingLevel [i] * 100 + Number.EPSILON ) / 100;
+	}
+
+	document.querySelector('main').insertAdjacentHTML('afterend','<div class = "river_chart"><canvas id="chart"></canvas></div>');
+	ctx = document.getElementById('chart').getContext('2d');
+	lineChart = new Chart(ctx, {
+		type: 'line',
+		data: {
+			labels: labels,
+			datasets: [{
+				label: 'динамика роста уровня реки',
+				backgroundColor: 'rgba(0, 255, 255, 0.9)',
+				data: drawingLiftingLevel
+			}, {
+				label: 'критический уровень',
+				borderColor: 'rgba(249, 128, 53, 0.9)',
+				pointRadius: 0,
+				data: drawingCriticalLevel
+			}]
+
+		}
+	});
 }
