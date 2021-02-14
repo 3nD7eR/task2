fd219d63c6244419deb763fc8b0799a82530dca7
3nD7eR <erikod0202@gmail.com>
2021-02-14 09:24:19 +0200

    added timing

--- js/main.js
+++ js/main.js
@@ -18,52 +18,56 @@ document.querySelector('button').addEventListener('click', function() {
 	if (elementDiv != null) {
 		elementDiv.remove();
 	}
+	elementDiv = document.querySelector('.block3');
+	if (elementDiv != null) {
+		elementDiv.remove();
+	}
 
 // 	ввод и проверка данных
 	inputNumber1 = document.getElementById('numberIn1').value;
 	inputNumber1 = Number(inputNumber1);
 	if (isNaN(inputNumber1) === true && typeof inputNumber1 != 'number') {
-		alert('input: 1,2,3,4,5,6,7,8,9,10');
+		alert('введите: 1,2,3,4,5,6,7,8,9,10');
 		return;
 	}
 	if (inputNumber1 <= 0 || inputNumber1 > 10) {
-		alert('input: 1,2,3,4,5,6,7,8,9,10');
+		alert('введите: 1,2,3,4,5,6,7,8,9,10');
 		return;
 	}
 	if (Number.isInteger(inputNumber1) === false) {
-		alert('input: 1,2,3,4,5,6,7,8,9,10');
+		alert('введите: 1,2,3,4,5,6,7,8,9,10');
 		return;
 	}
 	inputNumber2 = document.getElementById('numberIn2').value;
 	inputNumber2 = Number(inputNumber2);
 	if (isNaN(inputNumber2) === true && typeof inputNumber2 != 'number') {
-		alert('input: 10,20,30,40,50,60,70,80,90,100');
+		alert('введите: 10,20,30,40,50,60,70,80,90,100');
 		return;
 	}
 	if (inputNumber2 <= 0 || inputNumber2 > 100) {
-		alert('input: 10,20,30,40,50,60,70,80,90,100');
+		alert('введите: 10,20,30,40,50,60,70,80,90,100');
 		return;
 	}
 	if (inputNumber2 !== 10 && inputNumber2 !== 20 && inputNumber2 !== 30 && inputNumber2 !== 40 && inputNumber2 !== 50 && inputNumber2 !== 60 && inputNumber2 !== 70 && inputNumber2 !== 80 && inputNumber2 !== 90 && inputNumber2 !== 100) {
-		alert('input: 10,20,30,40,50,60,70,80,90,100');
+		alert('введите: 10,20,30,40,50,60,70,80,90,100');
 		return;
 	}
 	if (Number.isInteger(inputNumber2) === false) {
-		alert('input: 1 - 100');
+		alert('введите: 1 - 100');
 		return;
 	}
 	inputNumber3 = document.getElementById('numberIn3').value;
 	inputNumber3 = Number(inputNumber3);
 	if (isNaN(inputNumber3) === true && typeof inputNumber3 != 'number') {
-		alert('input: 1,2,3,4,5,6,7,8,9,10');
+		alert('введите: 1,2,3,4,5,6,7,8,9,10');
 		return;
 	}
 	if (inputNumber3 <= 0 || inputNumber3 > 10) {
-		alert('input: 1,2,3,4,5,6,7,8,9,10');
+		alert('введите: 1,2,3,4,5,6,7,8,9,10');
 		return;
 	}
 	if (Number.isInteger(inputNumber3) === false) {
-		alert('input: 1,2,3,4,5,6,7,8,9,10');
+		alert('введите: 1,2,3,4,5,6,7,8,9,10');
 		return;
 	}
 	if (inputNumber3 <= inputNumber1) {
@@ -92,9 +96,9 @@ function getDrawingPage() {
 	document.querySelector('body').insertAdjacentHTML('beforeend',
 		'<header>' +
 		'<h1>НАВОДНЕНИЕ</h1>' + '<h2>Введите параметры</h2>' +
-		'<h3>(1,2,3,4,5,6,7,8,9,10;           уровень реки)</h3>' +
-		'<h3>(10,20,30,40,50,60,70,80,90,100; процент поднятия уровня реки от прошлого уровня за один час)</h3>' +
-		'<h3>(1,2,3,4,5,6,7,8,9,10;           критический уровень реки)</h3>' +
+		'<h3>(1,2,3,4,5,6,7,8,9,10;           уровень реки м)</h3>' +
+		'<h3>(10,20,30,40,50,60,70,80,90,100; процент поднятия уровня реки от прошлого уровня за один час %)</h3>' +
+		'<h3>(1,2,3,4,5,6,7,8,9,10;           критический уровень реки м)</h3>' +
 		'<div class="input">' +
 		'<input id="numberIn1" type="text" autocomplete="off" placeholder="уровень реки"' +
 		' value=""/>' +
@@ -110,7 +114,7 @@ function getDrawingPage() {
 }
 
 function getDrawingResults(liftingLevel, criticalLevel) {
-	let lastLevel, index;
+	let lastLevel, index, criticalTimeMin;
 	let levelDynamics = [];
 
 	lastLevel = liftingLevel [liftingLevel.length - 1].toFixed(2);
@@ -118,7 +122,11 @@ function getDrawingResults(liftingLevel, criticalLevel) {
 		levelDynamics [index] = liftingLevel [index].toFixed(2);
 
 	}
-
+	criticalTimeMin = (criticalLevel - liftingLevel [liftingLevel.length - 2]) / ((liftingLevel [liftingLevel.length - 1] - liftingLevel [liftingLevel.length - 2]) / 60);
+	criticalTimeMin = criticalTimeMin.toFixed(0);
+/*	console.log(liftingLevel [liftingLevel.length - 2]);
+	console.log(criticalTimeMin);
+*/
 	document.querySelector('main').insertAdjacentHTML('beforeend',
-		`<p class="block1">Динамика роста уровня реки ${levelDynamics}</p>\<p class="block2">Через ${liftingLevel.length - 1} ч уровень реки превысит критический уровень ${criticalLevel} и станет ${lastLevel}, если не прорвет дамбу &#128526</p>`);
+		`<p class="block1">Динамика роста уровня реки ${levelDynamics}</p>\<p class="block2">Через ${liftingLevel.length - 2} ч ${criticalTimeMin} мин уровень реки достигнит критического уровня ${criticalLevel} м &#9888</p>\<p class="block3">Через ${liftingLevel.length - 1} ч уровень реки превысит критический уровень ${criticalLevel} м и станет ${lastLevel} м, если не прорвет дамбу &#128526</p>`);
 }
